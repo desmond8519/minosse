b88a9abe0d3db13d991bfaefd6a2c9cbf594901d
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-09-29 10:58:58 +0200

    Now steps are loaded through single file.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('test', 'Run all tests.', function(cb) {
 gulp.task('test-cucumber', 'Run cucumber integration tests.', function(cb) {
     exec(
         './node_modules/cucumber/bin/cucumber.js test/*.feature ' +
-        '-r lib/steps/ -r test/steps/ -f pretty',
+        '-r lib/steps.js -r test/steps/ -f pretty',
         function(err, stdout, stderr) {
             if (err) {
                 console.log(stdout);
